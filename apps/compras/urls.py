ac323a9d5e3df9cef0ee503c76db0cbcff735a48
kevintoloza <tolozakevin96@gmail.com>
2020-12-04 01:09:30 -0600

    Requision 70%

--- apps/compras/urls.py
+++ apps/compras/urls.py
@@ -3,5 +3,9 @@ from apps.compras.views import *
 
 urlpatterns = [
     path('requisicion/', indexRequision, name = 'indexRequision'),
-    path('requision/generar/', generarRequision, name='generar_requisicion')
+    path('requisicion/aprobar', AprobarRequision, name = 'aprobarRequision'),
+    path('requisicion/generar/', generarRequision, name='generar_requisicion'),
+    path('requisicion/generar/articulos/<int:id_requisicion>', generarRequisionArti, name='agregar_articulo'),
+    path('requisicion/aprobar/<int:id_requisicion>/botton/', botonAprobar, name='boton_aprobar')
+
     ]
--- apps/compras/views.py
+++ apps/compras/views.py
@@ -1,30 +1,61 @@
 from django.shortcuts import render, redirect
 from apps.compras.models import *
 from apps.articulos.models import *
+from apps.personas.models import *
+from django.contrib.auth.models import User
+
 # Create your views here.
 def indexRequision(request):
-    requisioncreadas = RequesionCompra.objects.all() # aca filtrar por el usuario
+    user = request.user.id
+    emple = Empleado.objects.get(auth_id=user)
+    requisioncreadas = RequesionCompra.objects.filter(id_departamento=emple.id_departamento.id, estado="Solicitado")#valorar si hacer una relacion con empleado para ver solo el que el usuario crea
     contexto = {'requision': requisioncreadas}
+    print(emple.id_departamento.id)
     return render(request,'requision/index.html', contexto)
 
 def generarRequision(request):
-    if request.method == 'GET':
-        articulos = Articulo.objects.all()
-        contexto = {'articulos' : articulos}
-        return render(request, 'requision/generar.html', contexto)
+    if request.method == 'POST':
+        user = request.user.id
+        empleado = Empleado.objects.get(auth_id=user)
     
-    elif request.method == 'POST':
-        articulos = Articulo.objects.filter(id__in = request.POST.getlist('articulos'))
         new_requision = RequesionCompra()
         new_requision.fecha_entrega = request.POST.get('fecha_entrega')
         new_requision.fecha_pedido = request.POST.get('fecha_pedido')
-        new_requision.cantidad_pedido = request.POST.get('cantidad_pedido')
         new_requision.estado = 'Solicitado'
-        new_requision.id_departamento_id = 1
+        new_requision.id_departamento_id = empleado.id_departamento.id
         new_requision.save()
-        if len(articulos) > 0:
-            new_requision.articulos.add(*articulos)
         return redirect('indexRequision')
+    else:
+        user = request.user.id
+        empleado = Empleado.objects.get(auth_id=user)
+        contexto = {'empleado': empleado
+                    }
+        return render(request, 'requision/generar.html', contexto) 
+
+def generarRequisionArti(request, id_requisicion):
+    if request.method == 'POST':
+        Requesion = RequesionCompra.objects.get(pk=id_requisicion)
+        new_rarticulo = RequesicionArticulo()
+
+    else:
+        return render(request,'requision/agregar_articulo.html')
+
+def AprobarRequision(request):
+    user = request.user.id
+    emple = Empleado.objects.get(auth_id=user)
+    requisioncreadas = RequesionCompra.objects.filter(id_departamento=emple.id_departamento.id)#valorar si hacer una relacion con empleado para ver solo el que el usuario crea
+    contexto = {'requision': requisioncreadas}
+    return render(request,'requision/aprobar.html',contexto)
+
+def botonAprobar(request,id_requisicion):
+    if request.method =='GET':
+        requi = RequesionCompra.objects.get(id=id_requisicion)
+        requi.estado = "Aprobado"
+        requi.save()
+        return redirect('aprobarRequision')
+
+    
+
 
 
 
